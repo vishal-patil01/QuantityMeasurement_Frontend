6b8f9d464ce1048eb358b27726b0da556d72fdda
vishal-patil01 <vishalpatil01@hotmail.com>
2020-04-29 14:09:42 +0530

    Added:TextField & DropDownList For SubUnit Conversion

--- src/components/QuantityMeasurement.js
+++ src/components/QuantityMeasurement.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import '../App.css';
+import TextField from '@material-ui/core/TextField';
 import CustomSelector from './CustomSelector';
 import { getMainUnits} from "../services/QuantityMeasurmentService";
 
@@ -11,8 +12,13 @@ export default class QuantityMeasurement extends React.Component {
         super(props);
         this.state = {
             mainUnitsList: [],
+            subUnitsList: [],
+            firstTextFieldValue: "",
+            secondTextFieldValue: "",
         };
         this.selectedUnits = "";
+        this.firstSubUnit = "";
+        this.secondSubUnit = "";
     }
 
     getMainUnits = () => {
@@ -34,6 +40,22 @@ export default class QuantityMeasurement extends React.Component {
                     <div className="Component-Container">
                         <CustomSelector width="450px" labelName="Main Units" onChange={this.getSubUnit}
                                         listData={this.state.mainUnitsList}/>
+                    </div>
+                    <div className="Component-Container">
+                        <TextField required id="outlined-required" variant="outlined"
+                                   helperText="Please Enter Value To Convert"
+                                   onChange={this.convertFromFirstToSecondUnit}
+                                   value={this.state.firstTextFieldValue}/>
+                        <span className="InlineDiv">=</span>
+                        <TextField required id="outlined-required" variant="outlined"
+                                   value={this.state.secondTextFieldValue} onChange={this.convertFromSecondToFirstUnit}
+                                   helperText="Please Enter Value To Convert"/>
+                    </div>
+                    <div className="Component-Container">
+                        <CustomSelector labelName="Sub Units" onChange={this.getFirstSubUnits}
+                                        listData={this.state.subUnitsList}/>
+                        <CustomSelector labelName="Sub Units" onChange={this.getSecondSubUnits}
+                                        listData={this.state.subUnitsList}/>
                     </div>
                 </CardContent>
             </Card>
